76924948906e4ce196d53a766a8ab3b151c299c2
im3zero <im3zero@gmail.com>
2026-09-24 13:55:20 +0900

    상단 가져지는 오류 해결

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -18,9 +18,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="flex flex-col min-h-dvh">
-      <div className="flex-1 pb-24">{children}</div>
-      <BottomNav />
-    </div>
+      <div className="flex flex-col min-h-dvh pt-[env(safe-area-inset-top)]">
+        <div className="flex-1 pb-24">{children}</div>
+        <BottomNav />
+      </div>
   )
 }
